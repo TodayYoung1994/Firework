a4e63c7d5be9d163571e8b3724ae05958bc663e5
Zheng Yang <zheng.yang@ndkey.com.cn>
2016-05-15 21:42:15 +0800

    complete test

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,8 +1,11 @@
+import cn.jing.exception.ModuleNotFoundException;
 import cn.jing.exception.NoFreeConnectionException;
 import cn.jing.manager.DefaultManager;
 import org.dom4j.DocumentException;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Properties;
@@ -25,7 +28,7 @@ public class Test {
         Connection c3 = manager.getConnection();
 
 
-        Thread.sleep(1000*30);
+        Thread.sleep(1000 * 30);
 
         Connection a1 = manager.getConnection();
         Connection a2 = manager.getConnection();
@@ -40,5 +43,31 @@ public class Test {
         //这个latch是为了阻止程序结束,这样就可以打开 http://localhost:8082/ 查看jmx
         CountDownLatch latch = new CountDownLatch(1);
         latch.await();
+    }
+
+    @org.junit.Test
+    public void testConnection() throws DocumentException, NoFreeConnectionException, ModuleNotFoundException, SQLException, InterruptedException {
+        DefaultManager manager = new DefaultManager();
+        //测试    module a
+        Connection a1 = manager.getConnection();
+        PreparedStatement p1 = a1.prepareStatement("select * from test");
+        ResultSet rs1 = p1.executeQuery();
+        while (rs1.next()) {
+            System.out.println(rs1.getString("value"));
+        }
+
+        System.out.println("=====================");
+        //测试    module b
+        Connection b1 = manager.getConnection("b");
+        PreparedStatement p2 = b1.prepareStatement("select * from test");
+        ResultSet rs2 = p1.executeQuery();
+        while (rs2.next()) {
+            System.out.println(rs2.getString("value"));
+        }
+
+        //
+        CountDownLatch latch = new CountDownLatch(1);
+        latch.await();
+
     }
 }
